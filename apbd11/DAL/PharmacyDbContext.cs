cbccca20b7eed9453f45161451a4cdfb9c64b66a
jszbm <jorgenszachowicz@gmail.com>
2025-05-25 19:47:55 +0200

    Initial commit

---
+++ apbd11/DAL/PharmacyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using apbd11.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apbd11.DAL;
+
+public class PharmacyDbContext : DbContext
+{
+    public DbSet<Doctor> Doctors { get; set; }
+
+    protected PharmacyDbContext()
+    {
+    }
+
+    public PharmacyDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+}
---
+++ apbd11/Models/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd11.Models;
+
+public class Doctor
+{
+    [Key]
+    public int IdDoctor { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; }
+    
+    [MaxLength(100)]
+    public string Email { get; set; }
+}
