1b903ef49212c86ef98177f26882704b71a9af46
jszbm <jorgenszachowicz@gmail.com>
2025-05-25 23:38:19 +0200

    Add current db snapshot

--- apbd11/Migrations/PharmacyDbContextModelSnapshot.cs
+++ apbd11/Migrations/PharmacyDbContextModelSnapshot.cs
@@ -48,6 +48,22 @@ namespace apbd11.Migrations
                     b.HasKey("IdDoctor");
 
                     b.ToTable("Doctors");
+
+                    b.HasData(
+                        new
+                        {
+                            IdDoctor = 1,
+                            Email = "john.doe@fake.com",
+                            FirstName = "John",
+                            LastName = "Doe"
+                        },
+                        new
+                        {
+                            IdDoctor = 2,
+                            Email = "jane.doe@fake.com",
+                            FirstName = "Jane",
+                            LastName = "Doe"
+                        });
                 });
 
             modelBuilder.Entity("apbd11.Models.Medicament", b =>
@@ -76,6 +92,22 @@ namespace apbd11.Migrations
                     b.HasKey("IdMedicament");
 
                     b.ToTable("Medicaments");
+
+                    b.HasData(
+                        new
+                        {
+                            IdMedicament = 1,
+                            Description = "It's Cope",
+                            Name = "Cope",
+                            Type = "Inhaler"
+                        },
+                        new
+                        {
+                            IdMedicament = 2,
+                            Description = "It's Music",
+                            Name = "Music",
+                            Type = "Injection"
+                        });
                 });
 
             modelBuilder.Entity("apbd11.Models.Patient", b =>
@@ -102,6 +134,146 @@ namespace apbd11.Migrations
                     b.HasKey("IdPatient");
 
                     b.ToTable("Patients");
+
+                    b.HasData(
+                        new
+                        {
+                            IdPatient = 1,
+                            Birthdate = new DateTime(1998, 6, 5, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            FirstName = "K",
+                            LastName = "N"
+                        },
+                        new
+                        {
+                            IdPatient = 2,
+                            Birthdate = new DateTime(1987, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            FirstName = "Gon",
+                            LastName = "Freecss"
+                        });
+                });
+
+            modelBuilder.Entity("apbd11.Models.Prescription", b =>
+                {
+                    b.Property<int>("IdPrescription")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("IdPrescription"));
+
+                    b.Property<DateTime>("Date")
+                        .HasColumnType("datetime2");
+
+                    b.Property<DateTime>("DueDate")
+                        .HasColumnType("datetime2");
+
+                    b.Property<int>("IdDoctor")
+                        .HasColumnType("int");
+
+                    b.Property<int>("IdPatient")
+                        .HasColumnType("int");
+
+                    b.HasKey("IdPrescription");
+
+                    b.HasIndex("IdDoctor");
+
+                    b.HasIndex("IdPatient");
+
+                    b.ToTable("Prescriptions");
+
+                    b.HasData(
+                        new
+                        {
+                            IdPrescription = 1,
+                            Date = new DateTime(2020, 9, 9, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            DueDate = new DateTime(2021, 7, 7, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            IdDoctor = 1,
+                            IdPatient = 1
+                        },
+                        new
+                        {
+                            IdPrescription = 2,
+                            Date = new DateTime(2015, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            DueDate = new DateTime(2018, 6, 6, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            IdDoctor = 2,
+                            IdPatient = 2
+                        });
+                });
+
+            modelBuilder.Entity("apbd11.Models.PrescriptionMedicament", b =>
+                {
+                    b.Property<int>("IdMedicament")
+                        .HasColumnType("int");
+
+                    b.Property<int>("IdPrescription")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Details")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
+
+                    b.Property<int>("Dose")
+                        .HasColumnType("int");
+
+                    b.HasKey("IdMedicament", "IdPrescription");
+
+                    b.HasIndex("IdPrescription");
+
+                    b.ToTable("PrescriptionsMedicaments");
+
+                    b.HasData(
+                        new
+                        {
+                            IdMedicament = 1,
+                            IdPrescription = 1,
+                            Details = "Inhale two times daily before meal",
+                            Dose = 5
+                        },
+                        new
+                        {
+                            IdMedicament = 2,
+                            IdPrescription = 2,
+                            Details = "Inject daily before sleep",
+                            Dose = 6
+                        });
+                });
+
+            modelBuilder.Entity("apbd11.Models.Prescription", b =>
+                {
+                    b.HasOne("apbd11.Models.Doctor", "Doctor")
+                        .WithMany()
+                        .HasForeignKey("IdDoctor")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("apbd11.Models.Patient", "Patient")
+                        .WithMany()
+                        .HasForeignKey("IdPatient")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Doctor");
+
+                    b.Navigation("Patient");
+                });
+
+            modelBuilder.Entity("apbd11.Models.PrescriptionMedicament", b =>
+                {
+                    b.HasOne("apbd11.Models.Medicament", "Medicament")
+                        .WithMany()
+                        .HasForeignKey("IdMedicament")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("apbd11.Models.Prescription", "Prescription")
+                        .WithMany()
+                        .HasForeignKey("IdPrescription")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Medicament");
+
+                    b.Navigation("Prescription");
                 });
 #pragma warning restore 612, 618
         }
